83b75472a7eee012058ee838567612b6c96af3d0
danfortunato <dan.fortunato@gmail.com>
2019-08-08 15:00:27 -0400

    Clean up construction of curved diffmats

--- util/curvedPoisson.m
+++ util/curvedPoisson.m
@@ -1,58 +1,81 @@
-function [uu, xx, yy] = curvedPoisson(f, x, y, varargin)
+function [uu, Dx, Dy, xx, yy] = curvedPoisson(f, x, y, n, varargin)
 %CURVEDPOISSON   Spectral Poisson solver in curvilinear coordinates.
 %
-%   CURVEDPOISSON(F, X, Y) solves Poisson's equation with righthand side F
-%   and zero Dirichlet boundary conditions using the coordinate
+%   CURVEDPOISSON(F, X, Y, N) solves Poisson's equation with righthand side
+%   F and zero Dirichlet boundary conditions using the coordinate
 %   transformations given by X = X(t,r) and Y(t,r), specified as a function
-%   handle or chebfun2. The default discretization is tensor-product
-%   Chebyshev.
+%   handle or chebfun2. The discretization is N x N tensor-product
+%   Chebyshev. The coordinate transformations X and Y are assumed to be
+%   defined on the (t,r)-domain [0 2*pi]x[0 1] if specified as function
+%   handles.
 %
-%   CURVEDPOISSON(F, X, Y, 'periodic') solves using a tensor-product
+%   CURVEDPOISSON(F, X, Y, [M N]) solves using an M x N discretization.
+%
+%   CURVEDPOISSON(F, X, Y, [M N], 'periodic') solves using a tensor-product
 %   Fourier-Chebyshev discretization.
 %
-%   CURVEDPOISSON(F, X, Y, N) solves using an N x N discretization.
+%   CURVEDPOISSON(F, X, Y, [M N], 'periodic', DBC, UBC) applies Dirichlet
+%   boundary conditions on the bottom and top given by the values in DBC
+%   and UBC.
 %
-%   CURVEDPOISSON(F, X, Y, M, N) solves using an M x N discretization.
+%   CURVEDPOISSON(F, X, Y, [M N], LBC, RBC, DBC, UBC) applies Dirichlet
+%   boundary conditions on the left, right, bottom, and top given by the
+%   values in LBC, RBC, DBC, and UBC.
 
 %% Parse arguments
-periodic = false;
-n = 21;
-m = n;
 dom = [0 2*pi 0 1];
+periodic = false;
 
-if ( isa(x, 'function_handle') )
-    x = chebfun2(x, dom);
-end
-if ( isa(y, 'function_handle') )
-    y = chebfun2(y, dom);
+if ( isa(x, 'chebfun2') && isa(y, 'chebfun2') )
+    if ( domainCheck(x, y) )
+        dom = x.domain;
+    else
+        error('X and Y must have the same domain.');
+    end
 end
 
-if ( nargin == 4 )
-    if ( isscalar(varargin{1}) )
-        n = varargin{1};
-        m = n;
-    else
-        periodic = strcmpi(varargin{1}, 'periodic');
-    end
-elseif ( nargin == 5 )
-    m = varargin{1};
-    n = m;
-    if ( isscalar(varargin{2}) )
-        n = varargin{2};
-    else
-        periodic = strcmpi(varargin{2}, 'periodic');
-    end
+if ( isscalar(n) )
+    % Call is CURVEDPOISSON(F, X, Y, N, ...)
+    m = n;
+else
+    % Call is CURVEDPOISSON(F, X, Y, [M N], ...)
+    m = n(1);
+    n = n(2);
+end
+
+lbc = zeros(m,1);
+rbc = zeros(m,1);
+dbc = zeros(n,1);
+ubc = zeros(n,1);
+
+if ( nargin == 5 )
+    % Call is CURVEDPOISSON(F, X, Y, [M N], 'periodic')
+    periodic = strcmpi(varargin{1}, 'periodic');
 elseif ( nargin == 6 )
-    m = varargin{1};
-    n = varargin{2};
-    periodic = strcmpi(varargin{3}, 'periodic');
+    % Call is CURVEDPOISSON(F, X, Y, [M N], 'periodic', DBC)
+    periodic = strcmpi(varargin{1}, 'periodic');
+    dbc = varargin{2};
+    ubc = dbc;
+elseif ( nargin == 7 )
+    % Call is CURVEDPOISSON(F, X, Y, [M N], 'periodic', DBC, UBC)
+    periodic = strcmpi(varargin{1}, 'periodic');
+    dbc = varargin{2};
+    ubc = varargin{3};
+elseif ( nargin == 8 )
+    % Call is CURVEDPOISSON(F, X, Y, [M N], LBC, RBC, DBC, UBC)
+    lbc = varargin{1};
+    rbc = varargin{2};
+    dbc = varargin{3};
+    ubc = varargin{4};
+end
+
+% Check compatibility at corners
+if ( ~periodic && ~(lbc(1) == ubc(1) && lbc(m) == dbc(1) && ...
+                    rbc(1) == ubc(n) && rbc(m) == dbc(n)) )
+   error('Boundary conditions are incompatible.');
 end
 
 %% Set up grids
-if ( periodic )
-    n = n-1;
-end
-
 if ( periodic )
     t = trigpts(n, dom(1:2));
 else
@@ -61,42 +84,14 @@ end
 r = chebpts(m, dom(3:4));
 [tt,rr] = meshgrid(t,r);
 
-xx = x(tt,rr);
-yy = y(tt,rr);
-ff = f(xx,yy);
-
-%% Compute the Jacobian
-dxdt = feval(diff(x,1,2), tt, rr);
-dxdr = feval(diff(x,1,1), tt, rr);
-dydt = feval(diff(y,1,2), tt, rr);
-dydr = feval(diff(y,1,1), tt, rr);
-J = dxdr.*dydt - dxdt.*dydr;
-
-% Compute d[t,r]/d[x,y] as the inverse of the Jacobian
-dtdx = -dydr ./ J;
-dtdy =  dxdr ./ J;
-drdx =  dydt ./ J;
-drdy = -dxdt ./ J;
-
 %% Construct operators
-
-sclt = 2/diff(dom(1:2));
-sclr = 2/diff(dom(3:4));
-
 if ( periodic )
-    Dt = sclt * diffmat(n, 'periodic');
+    [Dx, Dy, xx, yy] = curvedDiffmat(x, y, [m n], 'periodic');
 else
-    Dt = sclt * diffmat(n);
+    [Dx, Dy, xx, yy] = curvedDiffmat(x, y, [m n]);
 end
-Dr = sclr * diffmat(m);
-
-Im = eye(m);
-In = eye(n);
-Dt2d = kron(Dt,Im);
-Dr2d = kron(In,Dr);
-Dx = diag(dtdx(:)) * Dt2d + diag(drdx(:)) * Dr2d;
-Dy = diag(dtdy(:)) * Dt2d + diag(drdy(:)) * Dr2d;
 L = Dx^2 + Dy^2;
+ff = f(xx,yy);
 
 %% Impose boundary conditions
 
@@ -108,28 +103,23 @@ else
 end
 
 % Remove boundary DOFs
-L(b,:) = [];
-L(:,b) = [];
-ff(b) = [];
+II = kron(eye(n),eye(m));
+BC = zeros(m,n);
+BC(:,1) = lbc;
+BC(:,n) = rbc;
+BC(1,:) = ubc;
+BC(m,:) = dbc;
+L(b,:) = II(b,:);
+ff(b) = BC(b);
 
 %% Solve
 u = L \ ff(:);
 
-% Add in the boundary conditions
-uu = zeros(m,n);
-if ( periodic )
-    uu(2:m-1,:) = reshape(u,m-2,n);
-    if ( nargout == 1 )
+uu = reshape(u,m,n);
+if ( nargout == 1 )
+    if ( periodic )
         uu = chebfun2(uu, dom, 'trigx');
     else
-        % Wrap the data
-        uu = [uu uu(:,1)];
-        xx = [xx xx(:,1)];
-        yy = [yy yy(:,1)];
-    end
-else
-    uu(2:m-1,2:n-1) = reshape(u,m-2,n-2);
-    if ( nargout == 1 )
         uu = chebfun2(uu, dom);
     end
 end
