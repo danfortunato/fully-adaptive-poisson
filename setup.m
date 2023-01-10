ba88ea10fa12ea68460ddbe6787fa6cbc31504a6
danfortunato <dan.fortunato@gmail.com>
2023-01-09 19:36:48 -0500

    Remove unnecessary submodules

--- setup.m
+++ setup.m
@@ -1,24 +1,14 @@
 function setup
 
-warnstate = warning('off', 'MATLAB:dispatcher:nameConflict');
-
 fprintf('Initializing path directories...')
 root = fileparts(mfilename('fullpath'));
 addpath(root)
-addpath([root '/bie'])
-addpath([root '/tests'])
 addpath([root '/scripts'])
 
 % External dependencies
 addpath([root '/external/treefun'])
 addpath([root '/external/inpoly'])
 addpath([root '/external/kdtree/toolbox'])
-addpath([root '/external/BIE2D/panels'])
-addpath([root '/external/BIE2D/kernels'])
-addpath([root '/external/BIE2D/utils'])
-addpath([root '/external/linequad/matlab/bin'])
-addpath([root '/external/linequad/matlab/external/legtools'])
-addpath(genpath([root '/external/linequad/matlab/src']))
 
 if ( isempty(which('chebfun')) )
     error('Chebfun not found. Please install chebfun.');
@@ -29,7 +19,5 @@ if ( isempty(which('fmm2d')) )
 end
 
 fprintf(' done.\n');
-
-warning(warnstate)
 
 end
